795f92d650c361377b6567d52549a75f47b7d597
Katczajk <43179276+Katczajk@users.noreply.github.com>
2021-09-03 12:26:27 +0200

    added files

--- tutorial.py
+++ tutorial.py
@@ -5,10 +5,10 @@ def test1(text, alter):
 if __name__ == '__main__':
 
     # sample try catch similar to C++ / ABAP / C#
-    # here is not catch -> is execpt
-
+    # here is not catch -> is except
     try:
         test1("Ich bin", input('Alter eingeben: '))
+    # except work also in a function step
     except ValueError:
         test1("Ich bin", 30)
     input('Weiter mit einer Taste ...')
